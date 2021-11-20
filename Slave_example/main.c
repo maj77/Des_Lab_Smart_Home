e274fd5d629849cd3ca5552643ba208aa647c4dc
JZimnol <zimnol@student.agh.edu.pl>
2021-11-20 22:00:03 +0100

    initialize .c files

---
+++ Slave_example/main.c
@@ -0,0 +1,14 @@
+/*
+ * @file main.c
+ * @brief file containing simple slave device
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
+
